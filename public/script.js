7d9bdabe5131bf6a397f9f530c39d082c31d808f
Vansh Sharma <vansh.11919011622@ipu.ac.in>
2023-09-03 19:08:15 +0530

    script.js added

--- public/script.js
+++ public/script.js
@@ -1,29 +1,46 @@
-const form = document.querySelector('.box');
-const input = document.querySelector('.box__file');
-const uploading = document.querySelector('.box__uploading');
-const success = document.querySelector('.box__success');
-const error = document.querySelector('.box__error');
+window.onload = function() {
+  const fileInput = document.querySelector('.file-upload');
+  const label = document.querySelector('label[for="file-upload"]');
+  const selectedFiles = document.querySelector('.selected-files');
+  fileInput.addEventListener('change', displayFileName);
+  label.addEventListener('click', removeFileSelection);
+  
+  function displayFileName() {
+    const fileName = fileInput.files[0].name;
+    const fileExt = fileName.split('.').pop();
+    selectedFiles.innerHTML = `
+      <span>${fileName}</span><br>
+      <center><span class="file-extension" >${fileExt} file</span></center>
+  
+    `;
+    label.classList.add('file-selected');
+    fileInput.setAttribute('disabled', '');
+  }
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const file = input.files[0];
-  const formData = new FormData();
-  formData.append('file', file);
+  function removeFileSelection() {
+    selectedFiles.innerHTML = '';
+    label.classList.remove('file-selected');
+    fileInput.removeAttribute('enabled');
+  }
+}
 
-  uploading.style.display = 'block';
+function removeFileSelection() {
+  const fileInput = document.querySelector('.file-upload');
+  const label = document.querySelector('label[for="file-upload"]');
+  fileInput.removeEventListener('change', displayFileName);
+  fileInput.value = '';
+  label.removeEventListener('click', removeFileSelection);
+  label.style.cursor = 'not-allowed';
+  label.style.opacity = '0.5';
+  label.style.pointerEvents = 'none';
+  selectedFiles.style.display = 'none';
+}
 
-  fetch('/upload', {
-    method: 'POST',
-    body: formData
-  })
-    .then(response => response.text())
-    .then(response => {
-      uploading.style.display = 'none';
-      success.style.display = 'block';
-    })
-    .catch(error => {
-      uploading.style.display = 'none';
-      error.style.display = 'block';
-      error.querySelector('span').textContent = error.message;
-    });
-});
+const uploadButton = document.getElementById("upload-button");
+            uploadButton.addEventListener("click", function() {
+            window.location.href = "/upload";
+            });
+const uploadedButton = document.getElementById("home");
+            uploadButton.addEventListener("click", function() {
+            window.location.href = "/";
+            });
